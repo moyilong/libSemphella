5b610edd7ae48c886374ec2159253ac53e39f6cb
moyilong <moyilong@foxmail.com>
2015-09-03 12:19:55 +0800

    优化代码，加入内核代码

--- libSemphella/libSemphella.cpp
+++ libSemphella/libSemphella.cpp
@@ -6,41 +6,6 @@
 using namespace std;
 #include "libSemphella.h"
 
-bool debug_stat_locked=false;
-
-#ifdef STATIC_DEBUG
-
-API bool get_debug_stat()()
-{
-    return DEFAULT_DEBUG_STAT;
-}
-
-API void set_debug_stat(){};
-#else
-
-bool debug_stat=DEFAULT_DEBUG_STAT;
-
-API bool get_debug_stat()
-{
-    return debug_stat;
-}
-
-API void set_debug_stat(bool stat)
-{
-    if (!debug_stat_locked)
-    debug_stat=stat;
-}
-#endif
-
-API void into_debug_mode()
-{
-#ifndef _WNO_OMP
-    omp_set_num_threads(1);
-#endif
-debug_stat_locked=true;
-debug_stat=true;
-}
-
 API void sZero(void *dest,int value, int len)
 {
 	char*_dest = (char*)dest;
--- libSemphella/libSemphella.h
+++ libSemphella/libSemphella.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "main.h"
-API bool get_debug_stat();
-API void set_debug_stat(bool stat);
+#include "kernel.h"
 API void sZero(void *dest, int value, int len);
 
-#define DEBUG_LINE if (get_debug_stat())
+#define DEBUG_LINE if (kernel.get_debug_stat())
