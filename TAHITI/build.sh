2fd9239ceaa580fed077ef7770f8d676eb9bf766
moyilong <moyilong@foxmail.com>
2015-09-11 22:35:15 +0800

    Signed-off-by: moyilong <moyilong@foxmail.com>

--- TAHITI/build.sh
+++ TAHITI/build.sh
@@ -4,12 +4,12 @@ MODDIR=$2
 COMPILER=$4
 LINK=
 
-CFLAGS=$3 -lSemphella -fpermissive -Wall -Wno-sign-compare  -lgomp -fopenmp -std=c++11 -fpermissive \
-	-Wall -D__LINUX__ -I$(pwd)/../../  -L$OUTDIR
+CFLAGS="$3 -lSemphella -fpermissive -Wall -Wno-sign-compare  -lgomp -fopenmp -std=c++11 -fpermissive \
+	-Wall -D__LINUX__ -I$(pwd)/../../  -L$OUTDIR"
 for file in $(ls | grep .cpp | grep -v \~) ;
 	do
 		LINK=$LINK $OUTDIR/$file.o
-		$4 $3 -c -o $OUTDIR/$file.o $file 
+		$4 $CFLAGS -c -o $OUTDIR/$file.o $file 
 	done
 mkdir -p $MODDIR	
 cd $MODDIR
@@ -18,7 +18,7 @@ mkdir -p $OUTDIR/mod
 for file in $(ls | grep .cpp | grep -v \~);
 	do
 		LINK=$LINK $OUTDIR/$file.mod.o
-		$4 $3 -c -o $OUTDIR/mod/$file.mod.o $file
+		$4 $CFLAGS -c -o $OUTDIR/mod/$file.mod.o $file
 	done
 	
 $4 -o $OUTDIR/TAHITI $LINK
