4b39f4311948bbf15a579d56ae9285c3d90c6c00
moyilong <moyilong@foxmail.com>
2015-09-11 22:43:51 +0800

    Signed-off-by: moyilong <moyilong@foxmail.com>

--- libSemphella/libSemphella.cpp
+++ libSemphella/libSemphella.cpp
@@ -5,10 +5,22 @@
 #include <omp.h>
 using namespace std;
 #include "libSemphella.h"
+#ifndef __LINUX__
+#include <Windows.h>
+#endif
 
 API void sZero(void *dest,int value, int len)
 {
 	char*_dest = (char*)dest;
 	for (int n = 0; n < len; n++)
 		_dest[n] = value;
+}
+
+API void esleep(unsigned long long ms)
+{
+#ifdef __LINUX__
+	usleep(ms);
+#else
+	Sleep(ms);
+#endif
 }
--- libSemphella/libSemphella.h
+++ libSemphella/libSemphella.h
@@ -2,13 +2,6 @@
 #include "main.h"
 API void sZero(void *dest,int value,int len);
 
-inline void esleep(unsigned long long ms)
-{
-#ifdef __LINUX__
-	usleep(ms);
-#else
-	esleep(ms);
-#endif
-}
+API void esleep(unsigned long long ms);
 
 #define DEBUG_LINE if (true)
