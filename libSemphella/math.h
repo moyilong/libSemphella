ca2f27741288414349abbbc41bc5ca1626b4fda4
moyilong <moyilong@foxmail.com>
2015-09-23 22:17:00 +0800

    fix compile
    Signed-off-by: moyilong <moyilong@foxmail.com>

--- libSemphella/math.h
+++ libSemphella/math.h
@@ -22,14 +22,4 @@ template<class T>inline T dZero(T n)
 }
 
 
-#ifndef __LINUX__
-template<class C>inline int get_type_len(C value)
-{
-	C temp;
-	memset(&temp, 0xFF, sizeof(C));
-	char buff[MAX_BUFF_SIZE] = { 0x00 };
-	sprintf(buff, "%d", temp);
-	return strlen(buff);
-}
-
 #define INT_LEN	4096
