10cb0773f13bba6a534b83d478135c78e21ea2de
moyilong <moyilong@foxmail.com>
2016-01-28 13:01:56 +0800

    Signed-off-by: moyilong <moyilong@foxmail.com>

--- libSemphella/utils.h
+++ libSemphella/utils.h
@@ -10,13 +10,10 @@ inline void ShowProcessBar(double percent, string display, char finish='=', char
 	string inprocessd;
 	int fs = bis*percent;
 	int is = bis-fs;
-	for (int n = 0; n < (int)max(fs, is); n++)
-	{
-		if (n <= fs)
-			finishd += finish;
-		if (n <= is)
-			inprocessd += inprocess;
-	}
+	for (int n = 0; n < fs; n++)
+		finishd += finish;
+	for (int n = 0; n < is; n++)
+		inprocessd += inprocess;
 	finishd += splite;
 	char buff[MAX_BUFF_SIZE] = { 0x00 };
 	sprintf(buff, "%d", (int)(percent * 100));
