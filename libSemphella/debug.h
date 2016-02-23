465c934d30cf582a44f9e9868e3c672b3410cc4c
unknown <>
2016-02-23 16:35:22 +0800

    Signed-off-by: unknown <羿龙吟风>

--- libSemphella/debug.h
+++ libSemphella/debug.h
@@ -2,13 +2,22 @@
 #include "kernel.h"
 #include "config.h"
 
+inline string __debug_strgetfilename(string filename)
+{
+	int last = 0;
+	for (int n = 0; n < filename.size(); n++)
+		if (filename.at(n) == '\\' || filename.at(n) == '/')
+			last = n;
+	return filename.substr(last);
+}
+
 #ifdef __ALLOW_DYMANIC_DEBUG
 #define DEBUG_LINE	if (KERNEL.GetDebugStat())
 #else
 #define DEBUG_LINE if(__DEFAULT_DEBUG_STAT)
 #endif
 
-#define debug DEBUG_LINE cout<<"["<<__FILE__<<"]["<<__LINE__<<"]"
+#define debug DEBUG_LINE cout<<"["<<__debug_strgetfilename(__FILE__)<<"]["<<__LINE__<<"]"
 
 #define DEBUG_CALL_TRACK_ARGMENT	string __file_name=__FILE__ ,int __file_line=__LINE__
 #define DEBUG_CALL_TRACK_ARGMENT_FUNC	string __file_name,int __file_line
