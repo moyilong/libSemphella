8d345ebd7c1090752d96bbab6579f70bf08a5853
unknown <>
2016-04-04 15:32:44 +0800

    Signed-off-by: unknown <羿龙吟风>

--- xCPU/stdafx.h
+++ xCPU/stdafx.h
@@ -5,13 +5,6 @@
 
 #pragma once
 
-#include "targetver.h"
-
-#include <stdio.h>
-#include <tchar.h>
-
-
-
 // TODO:  在此处引用程序需要的其他头文件
 #include <libSemphella/libSemphella.h>
 #include <libSemphella/main.h>
