85e6bedfd1442030979996320b70e22182265b22
unknown <>
2016-05-16 16:46:48 +0800

    Signed-off-by: unknown <羿龙吟风>

--- libSemphella/config.h
+++ libSemphella/config.h
@@ -2,7 +2,7 @@
 #ifndef __CONFIG_FROM_CC
 #define __ALLOW_DEBUG_STAT_CHANGE		//允许代码修改调试状态
 #define __ALLOW_DYMANIC_DEBUG			//允许动态修改调试状态
-#define __DEFAULT_DEBUG_STAT	false	//默认调试状态
+#define __DEFAULT_DEBUG_STAT	true	//默认调试状态
 //#define __WNO_OMP						//禁用OpenMP头
 #define __ALLOW_LOWMEM_PREFER			//使用低内存优化配置
 #define MTYPE	float					//数学库浮点类型
