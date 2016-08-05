2956c4e93b3affc3330cdeaf90c41b84ed81076c
unknown <>
2016-08-05 15:01:13 +0800

    Signed-off-by: unknown <羿龙吟风>

--- PT2/stdafx.h
+++ PT2/stdafx.h
@@ -5,13 +5,6 @@
 
 #pragma once
 
-#include "targetver.h"
-
-#include <stdio.h>
-#include <tchar.h>
-
-
-
 // TODO:  在此处引用程序需要的其他头文件
 #include <libSemphella/main.h>
 #include <libSemphella/string.h>
