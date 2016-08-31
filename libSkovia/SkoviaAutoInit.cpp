68153a55b7f6e37f356c0f8c57a334b20c7839c7
unknown <>
2016-08-31 21:49:16 +0800

    Signed-off-by: unknown <羿龙吟风>

--- libSkovia/SkoviaAutoInit.cpp
+++ libSkovia/SkoviaAutoInit.cpp
@@ -8,11 +8,13 @@ public:
 
 AINIT::AINIT()
 {
+#ifndef __LINUX__
 	debug << "Init Windows WSA" << endl;
 	WSAData wsa;
 	WORD word = MAKEWORD(2, 2);
 	WSAStartup(word, &wsa);
 	debug << "WSA Is Inited" << endl;
+#endif
 }
 
 AINIT ainit;
