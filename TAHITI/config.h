a46c342f460bb8eb150bb5cda8364c3960114024
unknown <>
2016-11-26 14:45:20 +0800

    remove TAHITI and libQr
    Signed-off-by: unknown <羿龙吟风>

--- TAHITI/config.h
+++
@@ -1,7 +0,0 @@
-//#define UNLOAD_MODULES		//禁用模块载入
-//#define UNLOAD_EVENT			//禁用事件载入
-//#define DISABLE_EVENT_TRIGGER	//禁用事件触发
-//#define USE_WHITE_TRANS		//使用明文传输
-//#define ALLOW_ENDIAN_MISMATCH	//允许架构不匹配
-//#define ALLOW_API_MISMATCH	//允许API不匹配
-#define EXEC_HEAD_CHECK			//执行数据包检查
--- TAHITI/serverutil.h
+++
@@ -1,3 +0,0 @@
-#define SERVER_INFO_CLIENT_DEF	0xFFFF994D
-void ServerInfoClient(struct DATA_FORMAT in, struct DATA_FORMAT &out, SOCKET& conn);
-int ServerInfoCLI(vector<string>arg);
