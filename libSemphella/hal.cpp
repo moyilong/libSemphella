972d13b024c62d3bea292aa5f32b7836617c6699
unknown <>
2017-03-09 23:19:12 +0800

    Signed-off-by: unknown <羿龙吟风>

--- libSemphella/hal.cpp
+++ libSemphella/hal.cpp
@@ -4,6 +4,7 @@
 int UART0_Open(int fd, char* port);
 void UART0_Close(int fd);
 int UART0_Set(int fd, int speed, int flow_ctrl, int databits, int stopbits, int parity);
+int UART0_Init(int fd, int speed, int flow_ctrl, int databits, int stopbits, int parity);
 int UART0_Recv(int fd, char *rcv_buf, int data_len);
 int UART0_Send(int fd, char *send_buf, int data_len);
 #endif
@@ -106,7 +107,7 @@ void Serial::open()
 		parity_mode = 'N';
 		break;
 	}
-	int value = UART0_Set(handle, cfg.speed, 0, cfg.data_bit, cfg.stop_bit, parity_mode);
+	int value = UART0_Init(handle, cfg.speed, 0, cfg.data_bit, cfg.stop_bit, parity_mode);
 	debug << "Return Value:" << value << endl;
 	if (!value)
 	{
