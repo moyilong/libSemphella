8354bbc20040071eeb80a5752d3de9ac9e967ced
moyilong <moyilong@foxmail.com>
2017-08-13 11:02:45 +0800

    update CodeAlign

---
+++ CSemphella/utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSemphella
+{
+    public static class utils
+    {
+        public static bool in_array<T>(T [] get,T find)
+        {
+            bool stat = false;
+            Parallel.ForEach(get, who =>
+             {
+                 if (who.Equals(find))
+                     stat = true;
+             });
+            return stat;
+        }
+    }
+}
