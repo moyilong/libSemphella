732ee520f618c0d64dda702d287a8ddf505631e7
unknown <moyil@DESKTOP-O374P2N.(none)>
2017-09-18 11:15:11 +0800

    remove trash project
    Signed-off-by: unknown <moyil@DESKTOP-O374P2N.(none)>

--- EmuSPI/EMU_EEPROM.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <libSemphella/main.h>
-#include <libSemphella/string.h>
-#include <libSemphella/utils.h>
-#include <libSemphella/argment.h>
-#include <libSemphella/files.h>
--- msend/perfect.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <libSemphella/string.h>
-#include <libSemphella/net.h>
-#include <libSemphella/debug.h>
-#include <libSemphella/files.h>
-#include <libSemphella/crypt.h>
-#include <libSemphella/argment.h>
